47db96170cf7dc73dfb138df76c7cafa6b807a63
NahomHailu12 <hailunahom47@gmail.com>
2025-07-12 23:39:12 +0300

    final modification done

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const form = document.getElementById("hidden");
-form.style.display = "none";
+let indicator = false;
+var temp = 0;
 let id = 0;
 
+const table = document.getElementById("table");
 const btnSucess = document.getElementById("addTodo");
+
+form.style.display = "none";
 btnSucess.addEventListener("click", (e) => {
-     form.style.display = "block";
-     form.id.value = id;
+    btnSucess.style.display = "none";
+    document.getElementById("button_submit").defaultValue="add";
+    form.style.display = "block";
 })
 
-let indicator = false;
-const table = document.getElementById("table");
-
-var temp = 0;
+//update specific row
 function updateTask(id){
     indicator = true;
     temp = id;
-    console.log(id);
+    document.getElementById("button_submit").defaultValue="Update";
     form.style.display = "block";
+}
+
+//delete specific row
+function deleteTask(dId){
+    let conform = window.confirm("Are you sure you want to delete this task?");
+    if (conform)
+        document.getElementById('table').removeChild(document.getElementById(`tr-${dId}`));
 }
 
 form.addEventListener("submit", (e) => {
@@ -27,6 +36,7 @@ form.addEventListener("submit", (e) => {
         "date": document.form.date.value
     }
 
+    // to create and add new element
     if(!indicator) {
                 const todo = document.createElement("tr");
                 todo.setAttribute("id", `tr-${id}`);
@@ -35,12 +45,13 @@ form.addEventListener("submit", (e) => {
                                     <td>${lists.task}</td>
                                     <td>${lists.date}</td>
                                     <td><button id="update-${id} " class = "btn btn-success" onclick="updateTask(${id})">Update</button></td>
-                                    <td><button id="delete-${id}" class = "btn btn-warning" onclick="document.getElementById('table').removeChild(document.getElementById('tr-${id}'))">Delete</button></td>
-                                    `;
+                                    <td><button id="delete-${id}" class = "btn btn-warning" onclick="deleteTask(${id})">Delete</button></td>`;
                 table.appendChild(todo);
+                btnSucess.style.display = "block";
                 id++;
                 }
     else{
+        // update the specific row
         let tempid = `tr-${temp}`;
         row = document.getElementById(tempid);
         row.innerHTML = `
@@ -48,7 +59,7 @@ form.addEventListener("submit", (e) => {
                             <td>${lists.task}</td>
                             <td>${lists.date}</td>
                             <td><button id="update-${temp} " class = "btn btn-success" onclick="updateTask(${temp})">Update</button></td>
-                            <td><button id="delete-${temp}" class = "btn btn-warning" onclick="document.getElementById('table').removeChild(document.getElementById('tr-${temp}'))">Delete</button></td>
+                            <td><button id="delete-${temp}" class = "btn btn-warning" onclick="deleteTask(${temp})">Delete</button></td>
                 `;
         indicator = false;
     }
